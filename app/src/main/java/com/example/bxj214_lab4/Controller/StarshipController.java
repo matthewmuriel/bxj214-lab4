c5e29ec0ffbc1eed79a5858661bd0d1b4f41eb6d
Matthew <feroshi1@gmail.com>
2023-04-02 11:00:19 -0500

    Updated view

--- app/src/main/java/com/example/bxj214_lab4/Controller/StarshipController.java
+++ app/src/main/java/com/example/bxj214_lab4/Controller/StarshipController.java
@@ -1,6 +1,7 @@
 package com.example.bxj214_lab4.Controller;
 
 import android.app.Activity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bxj214_lab4.Model.Starship;
@@ -10,6 +11,7 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Locale;
 
 public class StarshipController {
     private final Activity activity;
@@ -42,8 +44,15 @@ public class StarshipController {
             tokens = c.split(",");
             TextView name = (TextView) activity.findViewById(activity.getResources().getIdentifier("crewName"+counter,"id", activity.getPackageName()));
             TextView rank = (TextView) activity.findViewById(activity.getResources().getIdentifier("crewRank"+counter,"id", activity.getPackageName()));
+            ImageView profile = (ImageView) activity.findViewById(activity.getResources().getIdentifier("crewpic"+counter,"id", activity.getPackageName()));
             name.setText(tokens[0]);
             rank.setText(tokens[1]);
+
+            tokens[0] = tokens[0].toLowerCase();
+            tokens = tokens[0].split(" ");
+
+            profile.setImageResource(activity.getResources().getIdentifier(tokens[tokens.length-1],"id",activity.getPackageName()));
+
             counter++;
         }
 
